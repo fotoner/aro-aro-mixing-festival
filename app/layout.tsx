f865d96d9cdf49bed97f5632bad55afa9a3b4614
Hyeonjae Nam / Fotone <jaemuon5582@gmail.com>
2025-07-08 05:58:10 +0900

    메타데이터 제목 수정: '2025 아로아로 믹싱 페스티벌'에서 '로싱스 - 2025 아로아로 믹싱 페스티벌'로 변경

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,18 +2,18 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "2025 아로아로 믹싱 페스티벌",
+  title: "로싱스 - 2025 아로아로 믹싱 페스티벌",
   description: "아로아로 애니송 이벤트 주최 온라인 믹싱 페스티벌",
   keywords: "아로아로, 믹싱, 페스티벌, 애니송, DJ, 공모, 로싱스, 애니메이션",
   openGraph: {
-    title: "2025 아로아로 믹싱 페스티벌",
+    title: "로싱스 - 2025 아로아로 믹싱 페스티벌",
     description: "아로아로 애니송 클럽 주최 믹싱 페스티벌",
     type: "website",
     locale: "ko_KR",
   },
   twitter: {
     card: "summary_large_image",
-    title: "2025 아로아로 믹싱 페스티벌",
+    title: "로싱스 - 2025 아로아로 믹싱 페스티벌",
     description: "아로아로 애니송 클럽 주최 믹싱 페스티벌",
   },
 };
